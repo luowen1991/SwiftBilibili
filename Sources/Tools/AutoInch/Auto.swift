a29f8a1c16c5e5de0afdd2f54309390282fdbb1f
luowen <luowen@dushu.io>
2020-09-27 16:37:06 +0800

    添加适配类

---
+++ Sources/Tools/AutoInch/Auto.swift
@@ -0,0 +1,9 @@
+//
+//  Auto.swift
+//  SwiftBilibili
+//
+//  Created by luowen on 2020/9/27.
+//  Copyright © 2020 luowen. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/Tools/AutoInch/Inch.swift
@@ -0,0 +1,9 @@
+//
+//  Inch.swift
+//  SwiftBilibili
+//
+//  Created by luowen on 2020/9/27.
+//  Copyright © 2020 luowen. All rights reserved.
+//
+
+import Foundation
